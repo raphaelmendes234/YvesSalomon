ba27b547fb75ff78f25620d9f66e3eaba5026797
ben212001 <benjamin.puzenat@gmail.com>
2023-06-09 16:51:44 +0200

    date_de_naissance_inachevé

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function moveUnderline(){
 moveUnderline();
 
 
-// Cacher le label quand l'input est rempli
+// Cacher le label quand l'input est rempli sauf pour le champ "date de naissance"
 
 const formField = document.querySelectorAll(".to-hide");
 const formsArray = Array.from(formField);
@@ -33,7 +33,6 @@ const formsArray = Array.from(formField);
 formsArray.map(function(element){
     const input = element.querySelector("input[type='text']");
     const label = element.querySelector("label");
-
 
     input.addEventListener("input", function(){
         if (input.value.length > 0) {
@@ -44,6 +43,21 @@ formsArray.map(function(element){
     });
 });
 
+// Cacher le label quand l'input est rempli que pour le champ "date de naissance"
+
+const formFieldDate = document.querySelector(".to-hide-date");
+const inputDate = formFieldDate.querySelector("input[type='date']");
+const labelDate = formFieldDate.querySelector("label");
+inputDate.previousSibling.size = "0";
+console.log(inputDate);
+
+inputDate.addEventListener('input', function() {
+    if (inputDate.value[0] == "1" || inputDate.value[0] == "2") {
+        labelDate.style.display = "none";
+    } else {
+        labelDate.style.display = "block";
+    }
+});   
 
 // Bouton
 
